bb06f2a646f9061423cf7db32d10d9868a3958f2
Theophane Durand <theophane.durand@etu.utc.fr>
2020-05-21 17:44:03 +0200

    Update creationVoc.py

--- creationVoc.py
+++ creationVoc.py
@@ -2,6 +2,8 @@
 from typing import Dict, Tuple, List, Union
 from lib.gopherpysat import Gophersat
 
+N = 4 # Taille de la grille 
+
 ## Ligne à remplacer avec VOTRE emplacement et nom de l'exécutable gophersat :
 ## Attention ! Sous Windows, il faut remplacer les '\' par des '/' dans le chemin
 
@@ -17,3 +19,14 @@ def creationVoc(n) -> List[str] : #Création des variables pour les 5 éléments
             voc.append("P{}{}".format(i, j))
             voc.append("B{}{}".format(i, j))
     return voc
+
+if __name__ == "__main__":
+    
+    voc = creationVoc(N)
+    gs = Gophersat(gophersat_exec, voc)
+    gs.push_pretty_clause(["-P00"])
+    gs.push_pretty_clause(["-W00"])
+    print(gs.dimacs())
+    print(gs.solve())
+    print(gs.get_pretty_model())
+    print(gs.get_model())
