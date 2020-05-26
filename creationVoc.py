cfb244096d1368ad9d0eaa6a32f6e4b801c936a9
Theophane Durand <theophane.durand@etu.utc.fr>
2020-05-26 18:32:17 +0200

    Update creationVoc.py

--- creationVoc.py
+++ creationVoc.py
@@ -13,51 +13,51 @@ def creationVoc(n) -> List[str] : #Création des variables pour les 5 éléments
     voc = []
     for i in range(n):
         for j in range(n):
-            voc.append("W{}{}".format(i, j))
-            voc.append("S{}{}".format(i, j))
-            voc.append("G{}{}".format(i, j))
-            voc.append("P{}{}".format(i, j))
-            voc.append("B{}{}".format(i, j))
+            voc.append("W{}_{}".format(i, j))
+            voc.append("S{}_{}".format(i, j))
+            voc.append("G{}_{}".format(i, j))
+            voc.append("P{}_{}".format(i, j))
+            voc.append("B{}_{}".format(i, j))
     return voc
 
 def ajoutClausesBreeze(gs, i, j):
-    r = ["-B{}{}".format(i, j)]
+    r = ["-B{}_{}".format(i, j)]
     if(i-1 > 0):
-        gs.push_pretty_clause(["B{}{}".format(i, j), "-P{}{}".format(i-1, j)])
-        r.append("P{}{}".format(i-1, j))
+        gs.push_pretty_clause(["B{}_{}".format(i, j), "-P{}_{}".format(i-1, j)])
+        r.append("P{}_{}".format(i-1, j))
     if(i+1 < N):
-        gs.push_pretty_clause(["B{}{}".format(i, j), "-P{}{}".format(i+1, j)])
-        r.append("P{}{}".format(i+1, j))
+        gs.push_pretty_clause(["B{}_{}".format(i, j), "-P{}_{}".format(i+1, j)])
+        r.append("P{}_{}".format(i+1, j))
     if(j-1 > 0):
-        gs.push_pretty_clause(["B{}{}".format(i, j), "-P{}{}".format(i, j-1)])
-        r.append("P{}{}".format(i, j-1))
+        gs.push_pretty_clause(["B{}_{}".format(i, j), "-P{}_{}".format(i, j-1)])
+        r.append("P{}_{}".format(i, j-1))
     if(j+1 < N):
-        gs.push_pretty_clause(["B{}{}".format(i, j), "-P{}{}".format(i, j+1)])
-        r.append("P{}{}".format(i, j+1))
+        gs.push_pretty_clause(["B{}_{}".format(i, j), "-P{}_{}".format(i, j+1)])
+        r.append("P{}_{}".format(i, j+1))
     gs.push_pretty_clause(r)
     
 def ajoutClausesStench(gs, i, j):
-    r = ["-S{}{}".format(i, j)]
+    r = ["-S{}_{}".format(i, j)]
     if(i-1 > 0):
-        gs.push_pretty_clause(["S{}{}".format(i, j), "-W{}{}".format(i-1, j)])
+        gs.push_pretty_clause(["S{}_{}".format(i, j), "-W{}_{}".format(i-1, j)])
         r.append("W{}{}".format(i-1, j))
     if(i+1 < N):
-        gs.push_pretty_clause(["S{}{}".format(i, j), "-W{}{}".format(i+1, j)])
-        r.append("W{}{}".format(i+1, j))
+        gs.push_pretty_clause(["S{}_{}".format(i, j), "-W{}_{}".format(i+1, j)])
+        r.append("W{}_{}".format(i+1, j))
     if(j-1 > 0):
-        gs.push_pretty_clause(["S{}{}".format(i, j), "-W{}{}".format(i, j-1)])
-        r.append("W{}{}".format(i, j-1))
+        gs.push_pretty_clause(["S{}{}".format(i, j), "-W{}_{}".format(i, j-1)])
+        r.append("W{}_{}".format(i, j-1))
     if(j+1 < N):
-        gs.push_pretty_clause(["S{}{}".format(i, j), "-W{}{}".format(i, j+1)])
-        r.append("W{}{}".format(i, j+1))
+        gs.push_pretty_clause(["S{}_{}".format(i, j), "-W{}_{}".format(i, j+1)])
+        r.append("W{}_{}".format(i, j+1))
     gs.push_pretty_clause(r)
 
 if __name__ == "__main__":
     
     voc = creationVoc(N)
     gs = Gophersat(gophersat_exec, voc)
-    gs.push_pretty_clause(["-P00"])
-    gs.push_pretty_clause(["-W00"])
+    gs.push_pretty_clause(["-P0_0"])
+    gs.push_pretty_clause(["-W0_0"])
     print(gs.dimacs())
     print(gs.solve())
     print(gs.get_pretty_model())
